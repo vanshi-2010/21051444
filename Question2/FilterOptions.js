91cb4356164e46fa378014f772fe49aa50577d64
Vanshika Raj <135754394+vanshi-2010@users.noreply.github.com>
2024-05-08 13:12:46 +0530

    Create FilterOptions.js

---
+++ Question2/FilterOptions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FilterOptions() {
+  // Implement filtering options here
+  return (
+    <div className="filter-options">
+      {/* Add filter UI components */}
+    </div>
+  );
+}
+
+export default FilterOptions;
