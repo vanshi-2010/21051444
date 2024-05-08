5990fdcdf5640a04b73a8cee7ec9b07f0865c768
Vanshika Raj <135754394+vanshi-2010@users.noreply.github.com>
2024-05-08 13:09:36 +0530

    Create App.js

---
+++ Question2/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AllProductsPage from './pages/AllProductsPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={AllProductsPage} />
+        <Route path="/product/:productId" component={ProductDetailPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
